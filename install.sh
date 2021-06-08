6c6935fc7d9b2954350b0a9bf6f3d54f69c8d0f5
imcooder <imcooder@qq.com>
2021-06-08 21:31:35 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -5,13 +5,15 @@ set -x
 set -e
 #set -o pipefail
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-BUNDLE_DIR="~/.vim/bundle"
+VIM_DIR="$HOME/.vim"
+BUNDLE_DIR="$VIM_DIR/bundle"
 echo "mkdir .vim"
-mkdir ~/.vim
+mkdir -p $BUNDLE_DIR || true
 echo "source dir ${BIN_DIR}"
 ln -sf "${BIN_DIR}/vimrc" ~/.vimrc
 echo "install bundle"
 mkdir -p "${BUNDLE_DIR}"
+cp -r ./vim/* $VIM_DIR
 cd ${BUNDLE_DIR}
 git clone https://github.com/VundleVim/Vundle.vim.git
 echo "finish"
